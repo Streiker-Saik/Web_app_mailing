09ceef281fa62bcaf5cd001c0c51fdc40c6281e7
Alexei <streiker.saik@gmail.com>
2025-07-15 00:26:42 +0300

    update config

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,15 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+from config import settings
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("client_connect/", include("client_connect.urls", namespace="client_connect")),
+    path("", include("client_connect.urls", namespace="client_connect")),
+    path("users/", include("users.urls", namespace="users")),
+    # path("client_connect/", include("client_connect.urls", namespace="client_connect")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
