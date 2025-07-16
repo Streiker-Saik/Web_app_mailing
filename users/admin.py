c5575e1ed680740baae1cf3367b88ee7bd420a0e
Alexei <streiker.saik@gmail.com>
2025-07-16 12:36:40 +0300

    update templates

--- users/admin.py
+++ users/admin.py
@@ -5,8 +5,18 @@ from .models import CustomUser
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
-    """Класс для работы администратора с кастомными пользователями"""
+    """
+    Класс для работы администратора с кастомными пользователями
+    Атрибуты:
+        ordering - сортировка по логику
+        list_filter - фильтрация активный пользователь или нет
+        exclude - исключит поле пароля
+        list_display - выводит на экран: логин, email, имя, фамилия, активный
+        search_fields - поиск по: логин, email
+    """
 
-    exclude = [
-        "password",
-    ]
+    ordering = ("username",)
+    list_filter = ("is_active",)
+    exclude = ("password",)
+    list_display = ("username", "email", "first_name", "last_name", "is_active",)
+    search_fields = ("username", "email",)
